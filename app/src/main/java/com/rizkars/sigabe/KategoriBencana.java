e28c4ae6cb64c59b467c1cfc8cefcb20fb34759a
rizkarahayu <rizkarahayu42@gmail.com>
2018-09-19 10:49:01 +0700

    Layout Quick

---
+++ app/src/main/java/com/rizkars/sigabe/KategoriBencana.java
@@ -0,0 +1,13 @@
+package com.rizkars.sigabe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KategoriBencana extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kategori_bencana);
+    }
+}
